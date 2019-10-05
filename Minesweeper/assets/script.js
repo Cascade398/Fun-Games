43560ca440f89ec56ae91428fb2172a59d742911
Tarun Kumar <tarunk0205@gmail.com>
2019-10-05 12:32:51 +0530

    Adding more functionality to the minefield.
    The previous version simply included the functionality to click and change the colors of the cells when they were clicked. This edit includes adding mines to this, hence it shows different colors when the mines are clicked.

--- Minesweeper/assets/script.js
+++ Minesweeper/assets/script.js
@@ -1,16 +1,43 @@
-var level = 64;
+var col = 8;
+var row = 8;
+var level = col*row;
 var n = 10;
 var tiles = document.querySelectorAll(".square");
 var mines = [];
 
+var mine = document.createAttribute("data-mine");
 
-for(var i=0;i<tiles.length;i++){
-  tiles[i].addEventListener("click",function(){
+function addmines(){
+  for(var i=0;i<n;i++){
+    mines.push(Math.floor(Math.random()*level));
+    tiles[mines[i]].status = 0;
+  }
+}
+
+function click(){
+  for(var i=0;i<level;i++){
+    tiles[i].addEventListener("click",clickCell);
+  }
+}
+
+function clickCell(e){
+  if(this.status == 0){
+    this.style.backgroundColor = "#DD0000";
+  }else{
     this.style.backgroundColor = "#CCCCCC";
-    this.textContent = "2";
-  });
+  }
 }
 
-for(var i=0;i<n;i++){
-  mines.push(Math.floor(Math.random()*81));
+function generate(){
+  for(var i=0;i<level;i++){
+    tiles[i] = { status: 1 };
+  }
+  addmines();
 }
+
+
+
+
+generate();
+click();
+
